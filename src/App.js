dfc69c3b5142189d6731472cddb3e7d9bd535697
AsanHO <gorma00@naver.com>
2022-07-05 22:06:22 +0900

    coinMarket

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [todo, setToDo] = useState("");
-  const [todos, setToDos] = useState([]);
-  const onChange = (e) => setToDo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setToDos((cur) => [todo, ...cur]);
-    setToDo(""); //제출이 완료되면 빈칸으로 업데이트
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((request) => request.json())
+      .then((json) => setCoins(json));
+    setLoading(false);
+  }, []);
 
   return (
     <div>
-      <h1>To Do List ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          type="text"
-          placeholder="WEWORK!"
-        />
-        <button>Add</button>
-      </form>
-      <hr />
-      {todos.map((item, index) => (
-        <li key={index}>{item}</li> //react에서 li태그는 index props를 가져야 함
-      ))}
+      <h1>The Coings!</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} {coin.symbol} : {coin.quotes.USD.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

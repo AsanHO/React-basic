5cd17488a9b88da2768ad563cd68ccde65e1c7dc
AsanHO <gorma00@naver.com>
2022-07-10 16:49:21 +0900

    dynamic route

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,12 @@
-function Movie({ coverImg, title, summary, genres }) {
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -13,4 +17,11 @@ function Movie({ coverImg, title, summary, genres }) {
   );
 }
 
+Movie.propTypes = {
+  id: propTypes.number.isRequired,
+  coverImg: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  genres: propTypes.arrayOf(propTypes.string).isRequired,
+};
 export default Movie;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoad] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovie = async () => {
+    const json = await await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+    setMovie(json.data.movie);
+    setLoad(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.large_cover_image} alt={movie.id} />
+          <h1>{movie.title}</h1>
+          <span>{movie.description_full}</span>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
